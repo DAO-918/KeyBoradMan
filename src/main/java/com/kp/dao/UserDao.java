a5d3a6f4176b5533f67ad2aa00d4ff82f511f4ad
“@shaowei2889680 <3206351387.com>
2019-11-13 15:27:12 +0800

    注册登录 找回密码 文章收藏暂时没有实现

--- src/main/java/com/kp/dao/UserDao.java
+++ src/main/java/com/kp/dao/UserDao.java
@@ -1,4 +1,16 @@
 package com.kp.dao;
 
+import com.kp.domain.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface UserDao {
+    public User login(User user);
+    void register(User user);
+    public User findUser(User user);
+    void updatePassword(User user);
+
+
+    public User findPhone(User user);
+
 }
